ceccd5a013fe15f112a005e3ef70e662322b7596
Lokesh chinatatareddy <lokeshchinatatareddy@Achiever-9.local>
2018-07-02 16:43:54 +0530

    3commit

--- gitcomments/ViewController.swift
+++ gitcomments/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 var name = "lokesh"
     var name1 = "vinod"
+    var name3 = "mahesh"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
